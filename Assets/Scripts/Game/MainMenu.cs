4b3e8dbfc6c3a36f717566a26e1eff1b65a6b13b
Janosz Zieliński <janosz@epicvr.pl>
2021-10-31 13:52:53 +0100

    add main menu

---
+++ Assets/Scripts/Game/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public int sceneIndex = 1;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
