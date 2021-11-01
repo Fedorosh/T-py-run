fd36d9e05ff489cc123dbcd5c9d10af28de70a02
Janosz Zieliński <janosz@epicvr.pl>
2021-11-01 12:13:56 +0100

    add sounds and sound enabling/disabling function

--- Assets/Scripts/Game/MainMenu.cs
+++ Assets/Scripts/Game/MainMenu.cs
@@ -14,4 +14,9 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void EnableDisableSound(bool sound)
+    {
+        AudioListener.pause = !sound;
+    }
 }
