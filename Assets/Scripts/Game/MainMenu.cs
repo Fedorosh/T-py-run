ec8e2e9762ffc6362bb125f4bd70b7d85187a352
Janosz Zieliński <janosz@epicvr.pl>
2021-11-21 19:52:03 +0100

    add deleting scores button

--- Assets/Scripts/Game/MainMenu.cs
+++ Assets/Scripts/Game/MainMenu.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
     public int sceneIndex = 1;
     private int soundOn;
     public Toggle sound;
+
+    [SerializeField] TextMeshProUGUI infoText;
     public void StartGame()
     {
         SceneManager.LoadScene(sceneIndex);
@@ -18,6 +21,19 @@ public class MainMenu : MonoBehaviour
     {
         soundOn = PlayerPrefs.GetInt("SoundOn", 1);
         sound.isOn = soundOn == 1 ? true : false;
+    }
+
+    public void ClearScores()
+    {
+        if(!PlayerPrefs.HasKey("HighScore"))
+        {
+            if (!infoText.gameObject.activeSelf) infoText.gameObject.SetActive(true);
+            infoText.text = "No Scores to delete.";
+            return;
+        }
+        PlayerPrefs.DeleteKey("HighScore");
+        if (!infoText.gameObject.activeSelf) infoText.gameObject.SetActive(true);
+        infoText.text = "Scores Cleared!";
     }
     public void QuitGame()
     {
